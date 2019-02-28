673fa910463854d050b29ec30c5dc4e1aa486eea
todd <todd@marops.com>
2019-02-28 17:26:53 -0500

    Added send mail to staff for signing up

--- hr3d/views.py
+++ hr3d/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 
 def signup(request):
     if request.method == 'POST':
@@ -11,6 +13,19 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+
+            emails = []
+            for u in User.objects.filter(is_staff=True):
+                emails.append(u.email)
+
+            send_mail(
+                'HR3D Signup',
+                'New user has signed up at HR3D.',
+                'noreply@hr3d.leidos.com',
+                emails,
+                fail_silently=False,
+            )
+
             return redirect('/')
     else:
         form = SignUpForm()
